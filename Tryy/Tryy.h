3b99b37f925cd798dbf764b22fc156bdde6ff52e
Doug Mead <dmead28@gmail.com>
2017-07-15 21:45:14 -0700

    Initial Commit

---
+++ Tryy/Tryy.h
@@ -0,0 +1,19 @@
+//
+//  Tryy.h
+//  Tryy
+//
+//  Created by Doug Mead on 7/15/17.
+//  Copyright © 2017 Doug Mead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tryy.
+FOUNDATION_EXPORT double TryyVersionNumber;
+
+//! Project version string for Tryy.
+FOUNDATION_EXPORT const unsigned char TryyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tryy/PublicHeader.h>
+
+
